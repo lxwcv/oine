838b0aa04a84249ae3f3d5f3819e745312dbb6d6
lxwc <1394926200@qq.com>
2020-11-13 09:40:02 +0800

    测试测试

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,8 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
     }
 }
