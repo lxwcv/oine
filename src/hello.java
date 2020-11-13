93448696b8daccee7f86a361032f8ab98347c542
落雪无尘 <49826487+lxwcv@users.noreply.github.com>
2020-11-13 01:45:03 +0000

    Update hello.java

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,9 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("我是云端的");
+        System.out.println("我是云端的");
+        System.out.println("我是云端的");
+        System.out.println("我是云端的");
+        System.out.println("我是云端的");
     }
 }
