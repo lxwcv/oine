0c758ebdd90e1174edba62bbfb838efb8f645edf
lxwc <1394926200@qq.com>
2020-11-13 09:43:58 +0800

    提交代码

--- src/hello.java
+++ src/hello.java
@@ -1,11 +1,5 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("我是云端的");
-         System.out.println("我是云端的");
-         System.out.println("我是云端的");
-         System.out.println("我是云端的");
-         System.out.println("我是云端的");
-         System.out.println("我是云端的");
-         System.out.println("我是云端的");
     }
 }
