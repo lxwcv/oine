fa691d4258825487478fae2667a649206e3beab6
落雪无尘 <49826487+lxwcv@users.noreply.github.com>
2020-11-13 01:41:49 +0000

    Update hello.java

--- src/hello.java
+++ src/hello.java
@@ -1,8 +1,5 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("1");
-        System.out.println("1");
-        System.out.println("1");
-        System.out.println("1");
+        System.out.println("我是云端的");
     }
 }
